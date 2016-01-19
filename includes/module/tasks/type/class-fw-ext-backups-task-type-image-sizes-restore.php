ed96b52d51e30414552262caadf973720796ba7e
Alexei Gogu <llemurya@gmail.com>
2016-01-19 17:44:36 +0200

    improve performance for image sizes restore task

--- includes/module/tasks/type/class-fw-ext-backups-task-type-image-sizes-restore.php
+++ includes/module/tasks/type/class-fw-ext-backups-task-type-image-sizes-restore.php
@@ -14,9 +14,10 @@ class FW_Ext_Backups_Task_Type_Image_Sizes_Restore extends FW_Ext_Backups_Task_T
 	 * @param array $args
 	 */
 	public function execute(array $args, array $state = array()) {
-		$backups = fw_ext('backups'); /** @var FW_Extension_Backups $backups */
+		$backups = fw_ext( 'backups' );
+		/** @var FW_Extension_Backups $backups */
 
-		if (empty($state)) {
+		if ( empty( $state ) ) {
 			$state = array(
 				// The attachment at which the execution stopped and will continue in next request
 				'attachment_id' => 0,
@@ -33,31 +34,27 @@ class FW_Ext_Backups_Task_Type_Image_Sizes_Restore extends FW_Ext_Backups_Task_T
 			"WHERE post_type = 'attachment' AND post_mime_type LIKE %s AND ID > %d",
 			"ORDER BY ID",
 			"LIMIT 7"
-		), " \n");
+		), " \n" );
 
-		$started_time = time();
-		$timeout = $backups->get_timeout() - 7;
 
-		while (time() - $started_time < $timeout) {
-			$attachments = $wpdb->get_results( $wpdb->prepare(
-				$sql, $wpdb->esc_like('image/').'%', $state['attachment_id']
-			), ARRAY_A );
+		$attachments = $wpdb->get_results( $wpdb->prepare(
+			$sql, $wpdb->esc_like( 'image/' ) . '%', $state['attachment_id']
+		), ARRAY_A );
 
-			if (empty($attachments)) {
-				return true;
-			}
-
-			foreach ($attachments as $attachment) {
-				if ( file_exists( $file = get_attached_file( $attachment['ID'] ) ) ) {
-					wp_update_attachment_metadata(
-						$attachment['ID'],
-						wp_generate_attachment_metadata( $attachment['ID'], $file )
-					);
-				}
-			}
-
-			$state['attachment_id'] = $attachment['ID'];
+		if ( empty( $attachments ) ) {
+			return true;
 		}
+
+		foreach ( $attachments as $attachment ) {
+			if ( file_exists( $file = get_attached_file( $attachment['ID'] ) ) ) {
+				wp_update_attachment_metadata(
+					$attachment['ID'],
+					wp_generate_attachment_metadata( $attachment['ID'], $file )
+				);
+			}
+		}
+
+		$state['attachment_id'] = $attachment['ID'];
 
 		return $state;
 	}
